769e1ce834f14b8cbff5721b0326bb6b1e7e9191
Ed Vidal Jr <develone@sbcglobal.net>
2023-02-14 15:53:28 -0700

    using define for WIFI_SSID & WIFI_PASSWORD

--- pico_w/wifi/freertos/iperf/pw_ssid.h
+++ pico_w/wifi/freertos/iperf/pw_ssid.h
@@ -1,8 +1,8 @@
 #ifndef _SSID_H
 #define _SSID_H
 
-#define WIFI_PASSWORD "t?bqxvcqh#6t"
+#define WIFI_PASSWORD "your ssid password"
 
-#define WIFI_SSID "ATTtpHTfPi"
+#define WIFI_SSID "your ssid"
 
 #endif
